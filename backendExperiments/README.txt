6867d8512100d3d8c02c4a05a0ddb8e8ddba0c50
Tenocci <sekelj13@highpoint.edu>
2016-04-12 10:11:11 -0400

    changed front-end to current

---
+++ backendExperiments/README.txt
@@ -0,0 +1,12 @@
+--JONATHAN SEKELA
+--4/11/2016
+
+  v8 holds the Google V8 library data. the folder,
+
+    USER-MADE-PROGS,
+
+  contains everything I've created to test the library.
+  To edit, go to that folder.
+
+  TO COMPILE:
+    node-gyp configure program.cpp
--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const electron = require('electron');
 const app = electron.app;  // Module to control application life.
 const BrowserWindow = electron.BrowserWindow;  // Module to create native browser window.
 
+
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 var mainWindow = null;
@@ -21,13 +22,13 @@ app.on('window-all-closed', function() {
 // initialization and is ready to create browser windows.
 app.on('ready', function() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600});
+  mainWindow = new BrowserWindow({width: 1400, height: 900});
 
   // and load the index.html of the app.
   mainWindow.loadURL('file://' + __dirname + '/index.html');
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  //mainWindow.webContents.openDevTools();
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function() {
