8fa95e673127302ddb2d657c4d38e08b920b904a
Tenocci <sekelj13@highpoint.edu>
2016-04-14 10:35:04 -0400

    added nodejs to scripts.js in backendExperiments (doesn't work yet)

--- backendExperiments/scripts.js
+++ backendExperiments/scripts.js
@@ -1,5 +1,5 @@
 /* Jonathan Sekela
- * 4/13/2016
+ * 4/14/2016
  * javascripts for backendExperiments with c++ and nodeJS
  */
 
@@ -7,3 +7,20 @@ function execute()
 {
   $("#outMsg").html("what do i put here lol");
 }
+
+
+
+const spawn = require('child_process').spawn;
+const ls = spawn('hello.exe', ['-lh', '/usr']);//changed 'ls' to 'hello.exe'
+
+ls.stdout.on('data', (data) => {
+  console.log(`stdout: ${data}`);
+});
+
+ls.stderr.on('data', (data) => {
+  console.log(`stderr: ${data}`);
+});
+
+ls.on('close', (code) => {
+  console.log(`child process exited with code ${code}`);
+});
