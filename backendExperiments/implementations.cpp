4261136bece479c0eb0de0598586eaf9181dc955
Tenocci <sekelj13@highpoint.edu>
2016-04-18 17:52:33 -0400

    edited node object code

--- backendExperiments/implementations.cpp
+++ backendExperiments/implementations.cpp
@@ -1,16 +1,52 @@
 #include <iostream>
 #include <string>
+#include "nodeObjArr.h"
+#include "nodeObj.h"
 
 using namespace std;
 
-/*******************nodeType*********************/
-nodeType::nodeType()
-{
 
+/*****************************nodeType*******************************/
+nodeType::nodeType(double active,double val)
+{
+  setActivation(active);
+  setPriming(val);
+  setProbability();
+}
+
+void nodeType::setActivation(double active)
+{
+  
+}
+
+void nodeType::setPriming(int val)
+{
+  priming=val;
+}
+
+void nodeType::setProbabilty()
+{
+  probability=(activation+probability)/2;    //if weird values occur, CHECK
+                                             //AND MODIFY THIS FUNCTION
+}
+
+double nodeType::getActivation()
+{
+  return activation;
+}
+
+double nodeType::getPriming()
+{
+  return priming;
+}
+
+double nodeType::getProbability()
+{
+  return probability;
 }
 
 
-/*******************nodeArrType******************/
+/*****************************nodeArrType****************************/
 nodeArrType::nodeArrType(int nodeNum)
 {
 
--- backendExperiments/nodeObj.h
+++ backendExperiments/nodeObj.h
@@ -6,11 +6,31 @@ using namespace std;
 class nodeType
 {
 public:
-  nodeType();
+  nodeType(double active=0,double priming=.5);
+    //constructor
+
+  void setActivation(double active);
+    //sets activation to a value between 0 and 1:
+    //0 is inactive, 1 is completely active
+
+  void setPriming(double val);
+    //sets priming to a value between 0 and 1:
+    //this is the number that gets displayed
+    //in the circles on the page.
+
+  void setProbability();
+    //sets probability based on priming and activation
+
+  double getActivation();
+
+  double getPriming();
+
+  double getProbability();
+
 
 
 private:
-  int activated;
-  int value;
-  int probability;
+  int activation;        //increases probability when higher
+  double priming;        //increased when primed
+  double probability;    //determined by activation and priming together
 }
