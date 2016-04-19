e49d9c29646e9c7718706a42fa43cda1368c12f1
Tenocci <sekelj13@highpoint.edu>
2016-04-19 15:03:10 -0400

    changes

---
+++ backendExperiments/c++/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+#include "nodeType.h"
+#include "nodeArrType.h"
+
+using namespace std;
+
+int main()
+{
+  string inputPhrase;
+  cin << inputPhrase;
+  while (inputPhrase != "0") {
+    //code for running simulation goes here.
+    //basically, javascript calls the executable when
+    //the app is first started up, then this main
+    //waits for input from the javascript and outputs
+    //stuff to the javascript while it hasn't received
+    //the "terminate" instruction.
+
+    
+
+    cin << inputPhrase;
+  }
+  return 0;
+}
