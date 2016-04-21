c72436454759bff37af33e4fbc159d7a38fd5ea3
Tenocci <sekelj13@highpoint.edu>
2016-04-20 23:48:26 -0400

    changes to c++ folder

--- backendExperiments/c++/nodeArrType.h
+++ backendExperiments/c++/nodeArrType.h
@@ -13,10 +13,23 @@ public:
     //their probabilities, and calls their
     //prime() functions
 
+  void setNumOfNodes(int num=100);
+    //if num is even, sets numOfNodes equal
+    //to num. If not, sends an error to cerr
 
+  int getNumOfNodes();
+    //returns numOfNodes
 
+  void setNodeArray();
+    //requires numOfNodes to be initialized first
+    //sets up nodeArray with the correct number
+    //of nodes
+
+  nodeType* getNodeArray();
+
+  void prime();
 
 private:
   int numOfNodes;
-  nodeType* nodes;
+  nodeType* nodeArray;
 }
--- backendExperiments/c++/nodeArrTypeImp.cpp
+++ backendExperiments/c++/nodeArrTypeImp.cpp
@@ -11,6 +11,29 @@ nodeArrType::nodeArrType(int nodeNum)
 
 }
 
+void nodeArrType::setNumOfNodes(int num)
+{
+  if(!(num%2)) {
+    numOfNodes=num;
+  } else cerr << "Error: must have an even number of nodes";
+}
+
+int nodeArrType::getNumOfNodes()
+{
+  return numOfNodes;
+}
+
+void setNodeArray()
+{
+
+}
+
+nodeType* nodeArrType::getNodeArray()
+{
+  //don't know what should go here, since it's a pointer
+  //instead of a copiable value
+}
+
 nodeArrType::prime()
 {
 
