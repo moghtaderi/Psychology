b03e53f968d2c127fc8562ac254ac9d86a2059d7
Tenocci <sekelj13@highpoint.edu>
2016-04-22 11:33:32 -0400

    RANDOMIZATION WORKS

--- backendExperiments/c++/randomizationAlg.cpp
+++ backendExperiments/c++/randomizationAlg.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <vector>
 #include <random>
+#include <ctime>
 
 #include "nodeType.h"
 
@@ -13,13 +14,14 @@ void randomInit(double ratio,int numElem,nodeType nodes[]);
 int main()
 {
   nodeType nodes[40];
-  for(int i=0;i < 40;i++) {
-    cout << nodes[0].getWeight() << ' ';
+  for(int i=0;i < 20;i++) {
+    cout << nodes[i].getWeight() << ':' << nodes[i+20].getWeight() << endl;
   }
   cout << "\n\n\n";
   randomInit(.74,20,nodes);
-  for(int i=0;i < 40;i++) {
-    cout << nodes[0].getWeight() << ' ';
+  cout << "\n\n\n";
+  for(int i=0;i < 20;i++) {
+    cout << nodes[i].getWeight() << ':' << nodes[i+20].getWeight() << endl;
   }
   return 0;
 }
@@ -27,6 +29,7 @@ int main()
 //=======================================================================================
 void randomInit(double ratio, int numElem,nodeType nodes[])
 {
+  srand(time(NULL));
   vector<int> done1;
   vector<int> done2;
   //initialize done1 and done2 with numbers to be removed upon assignment of weight
@@ -42,12 +45,16 @@ void randomInit(double ratio, int numElem,nodeType nodes[])
   int currentElems=numElem;                      //number will be decremented, so is copy of numElem
 
   while(currentElems>0) {
+    cout << "CURRENT ELEMS: " << currentElems << endl;
     randPlace=rand() % currentElems;             //random integer between 0 and numElem
+    cout << "randPlace: " << randPlace << endl;
     randWeight1=(double)rand()/RAND_MAX;         //random double between 0 and 1
+    cout << "randWeight1: " << randWeight1 << endl;
 
     if((randWeight1/ratio)<=1)
       randWeight2=randWeight1/ratio;
     else randWeight2=randWeight1*ratio;
+    cout << "randWeight2: " << randWeight2 << endl;
 
     if(randWeight1>randWeight2) {
       nodes[done1[randPlace]].setWeight(randWeight1);
